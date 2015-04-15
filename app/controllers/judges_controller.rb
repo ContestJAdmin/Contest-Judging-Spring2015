355ce91c4207785a2d5f2dbac3710e7b41a8dae6
rawilder <rawilder0506@gmail.com>
2015-04-15 05:59:48 +0000

    collapsable display now works and populates properly

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -5,8 +5,16 @@ class JudgesController < ApplicationController
        @projects = Project.all
   end
   def show
-    @tests = User.joins(:projects).select('users.name as user_name, users.email, projects.name as project_name, projects.location, count(projects.id) as total_proj')
-    @jps = User.joins('u left outer join projects_users pj on pj.user_id=u.id').select('u.name as user_name, u.email, count(pj.project_id) as total_proj' ).where('u.id <> 1').group('pj.user_id,u.name,u.email')
-    @pjs = Project.joins('p left outer join projects_users pj on pj.project_id=p.id').select('p.name as proj_name, count(pj.user_id) as total_judge' ).group('pj.project_id,p.name')
+    @judges = User.all.where(admin: false)
+    @judges_projects = {}
+    @judges.each do |judge|
+        @judges_projects[judge.id] = Project.includes(:users).where('users.id' => judge.id)
+    end
+    
+    @projects = Project.all
+    @projects_judges = {}
+    @projects.each do |project|
+        @projects_judges[project.id] = User.includes(:projects).where('projects.id' => project.id)
+    end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
     belongs_to :category
     belongs_to :contest
-
+    
+    has_and_belongs_to_many :users
  #   has_and_belongs_to_many :users, :join_table => "projects_users", :foreign_key => "user_id"
 end
