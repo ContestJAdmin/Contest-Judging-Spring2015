b9008945291e8d4c488ab23496cf5d8b9a01f878
Matthew Kocmoud <matthew.kocmoud@neo.tamu.edu>
2015-04-17 03:52:40 +0000

    Fixed category use for projects

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -14,7 +14,6 @@ class Attachment < ActiveRecord::Base
       @project = contest.projects.build
       @project.name = row[0]
       @project.location = row[1]
-      @project.category_id = row[2]
       @project.save
       puts @project.inspect
     end
