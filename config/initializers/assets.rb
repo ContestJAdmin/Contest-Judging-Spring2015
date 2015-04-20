78cb415667cd7618b70640add3d3210828833913
rawilder <rawilder0506@gmail.com>
2015-04-20 05:01:00 +0000

    changed precompile to affect only files in the app/assets/javascript and stylesheets folders

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-Rails.application.config.assets.precompile = ['*.js', "*.js.coffee", '*.css', "*.css.scss"]
+Rails.application.config.assets.precompile << Rails.root.join('app', 'assets', 'javascripts', '*')
+Rails.application.config.assets.precompile << Rails.root.join('app', 'assets', 'stylesheets', '*')
