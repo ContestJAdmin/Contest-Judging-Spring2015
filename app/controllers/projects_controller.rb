e4975166e1aef9cce0dd33ca2b1ea486bf668006
rawilder <rawilder0506@gmail.com>
2015-04-22 14:03:51 +0000

    added page to view projects that have not been assigned a category and also fixed a testing bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,74 +1,79 @@
 class ProjectsController < ApplicationController
- before_filter :authenticate_user!
-  before_filter :has_contest, :only => [:new, :create, :destroy]
+  before_filter :authenticate_user!
+  before_filter :has_contest, :only => [:new, :create, :destroy, :unassigned]
 
-    protected
-    def has_contest
-        unless (@contest = Contest.find(params[:contest_id]))
-            flash[:error] = 'Project must be for an existing contest'
-            redirect_to contests_path
-        end
-    end    
-    
-    public
-    def index
-        @projects = current_user.projects
+  protected
+  def has_contest
+    unless (@contest = Contest.find(params[:contest_id]))
+      flash[:error] = 'Project must be for an existing contest'
+      redirect_to contests_path
     end
-
-    def new
-        @project = @contest.projects.build
-    end
+  end    
   
-    def show
-        if current_user.admin? then 
-            @project = Project.find(params[:id])
-        end
- #   unless @user == current_user || current_user.admin?
- #     redirect_to :back, :alert => "Access denied."
- #   end
-        if !current_user.admin? then
-            redirect_to :gradesheet
-        end   
-    end
-  
-    def create
-        @project = @contest.projects.build(project_params)
-        puts project_params
-        if @project.save
-            flash[:success] = "Successfull"
-            redirect_to @contest
-        else
-            render 'new'
-        end
-    end
+  public
+  def index
+    @projects = current_user.projects
+  end
 
-    def self.get_project_details(id)
-        return Project.find(id)
-    end
-    
-    def edit
-        @project = Project.find(params[:id])
-    end
-    
-    def update
-        @project = Project.find(params[:id])
-        if @project.update_attributes(project_params)
-            flash[:success] = "Project Updated"
-            redirect_to :back
-        else
-            render 'projects/edit'
-        end
-    end
-    
-    def destroy
-        @project = @contest.projects.find(params[:id])
-        @project.destroy
-        redirect_to contest_path(@contest)
-    end
+  def new
+    @project = @contest.projects.build
+  end
 
-    private
-  
-    def project_params
-            params.require(:project).permit(:name, :location, :category_id)
+  def show
+    if current_user.admin? then 
+      @project = Project.find(params[:id])
     end
+    # unless @user == current_user || current_user.admin?
+    #   redirect_to :back, :alert => "Access denied."
+    # end
+    if !current_user.admin? then
+      redirect_to :gradesheet
+    end   
+  end
+
+  def create
+    @project = @contest.projects.build(project_params)
+    puts project_params
+    if @project.save
+      flash[:success] = "Successfull"
+      redirect_to @contest
+    else
+      render 'new'
+    end
+  end
+
+  def self.get_project_details(id)
+    return Project.find(id)
+  end
+  
+  def edit
+    @project = Project.find(params[:id])
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      flash[:success] = "Project Updated"
+      redirect_to :back
+    else
+      render 'projects/edit'
+    end
+  end
+  
+  def unassigned
+    @projects = @contest.projects.where("category_id IS NULL")
+    render 'projects/unassigned'
+  end
+  
+  def destroy
+    @project = @contest.projects.find(params[:id])
+    @project.destroy
+    redirect_to contest_path(@contest)
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :location, :category_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-    resources :contests do
-        resources :categories
-        resources :attachments
-        resources :projects
+  get '/contests/:contest_id/projects/unassigned', to: 'projects#unassigned', :as => :unassigned_projects
+  resources :contests do
+    resources :categories
+    resources :attachments
+    resources :projects do 
     end
-    post '/contests/:contest_id/categories/:id/attach', to: 'categories#attach', :as => :attach
-    post '/contests/:contest_id/categories/:id/unattach', to: 'categories#unattach', :as => :unattach
-    root to: 'visitors#index'
-    devise_for :users
-    resources :users
-    resources :projects
-    resources :judges
-    resources :questions
-    resources :question_types
-    resources :gradesheets
-    resources :scores
+  end
+  post '/contests/:contest_id/categories/:id/attach', to: 'categories#attach', :as => :attach
+  post '/contests/:contest_id/categories/:id/unattach', to: 'categories#unattach', :as => :unattach
+  root to: 'visitors#index'
+  devise_for :users
+  resources :users
+  resources :projects
+  resources :judges
+  resources :questions
+  resources :question_types
+  resources :gradesheets
+  resources :scores
 end
